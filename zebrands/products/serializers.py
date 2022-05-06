ab9be34ff3e9e8b9c8d08cd09107139cd87b0be6
karimy chable <karimy@sunwise.io>
2022-05-06 07:43:04 -0500

    Validations on products, required fields and unique sku

--- zebrands/products/serializers.py
+++ zebrands/products/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from zebrands.products.models import Product
 
 
 class PostProductSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(required=True)
+    description = serializers.CharField(required=True)
+    sku = serializers.CharField(required=True, validators=[UniqueValidator(queryset=Product.objects.all())])
+
     class Meta:
         model = Product
         fields = '__all__'
